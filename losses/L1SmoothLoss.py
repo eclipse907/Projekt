d2d3a19fa97ed8d949ef226612d2d23ba9534f36
Pavo98 <Pavo.Matanovic@fer.hr>
2019-12-19 12:14:45 +0100

    Promijenjeni razredi Regularizer i LossFunction u abstraktne razrede i formatiranje

--- losses/L1SmoothLoss.py
+++ losses/L1SmoothLoss.py
@@ -1,40 +1,44 @@
 import numpy as np
 from losses.LossFunction import *
 
+
 class L1SmoothLoss(LossFunction):
 
-  def __init__(self, weights, weight_decay, name):
-    """
-    Args:
-      weights: parameters which will be regularized
-      weight_decay: lambda, regularization strength
-      name: layer name
-    """
-    # this is still a reference to original tensor so don't change self.weights
-    self.weights = weights
-    self.weight_decay = weight_decay
-    self.name = name
+    def __init__(self, weights, weight_decay, name):
+        """
+        Args:
+          weights: parameters which will be regularized
+          weight_decay: lambda, regularization strength
+          name: layer name
+        """
+        # this is still a reference to original tensor so don't change self.weights
+        self.weights = weights
+        self.weight_decay = weight_decay
+        self.name = name
 
-  def forward(self):
-    """
-     Returns:
-      Scalar, smooth l1 loss.
-    """
-    # smooth L1 loss
-    # (for abs(x) < 1/lambda) = 0.5 * lambda * sum(weight**2)
-    # (for abs(x) >= 1/lambda) = abs(weight)-0.5/lambda
-    lossAllWeights = np.where(np.abs(self.weights) < 1/self.weight_decay, self.weight_decay*0.5* np.square(self.weights), np.abs(self.weights)-0.5/self.weight_decay)
+    def forward(self):
+        """
+         Returns:
+          Scalar, smooth l1 loss.
+        """
+        # smooth L1 loss
+        # (for abs(x) < 1/lambda) = 0.5 * lambda * sum(weight**2)
+        # (for abs(x) >= 1/lambda) = abs(weight)-0.5/lambda
+        lossAllWeights = np.where(np.abs(self.weights) < 1 / self.weight_decay,
+                                  self.weight_decay * 0.5 * np.square(self.weights),
+                                  np.abs(self.weights) - 0.5 / self.weight_decay)
 
-    print(lossAllWeights)
-    return np.sum(lossAllWeights)
+        print(lossAllWeights)
+        return np.sum(lossAllWeights)
 
-  def backward_params(self):
-    """
-    Returns:
-      Gradient of the smooth L1 loss with respect to the weights.
-    """
+    def backward_params(self):
+        """
+        Returns:
+          Gradient of the smooth L1 loss with respect to the weights.
+        """
 
-    gradAllWeights =  np.where(np.abs(self.weights) < 1/self.weight_decay, self.weight_decay * self.weights, np.sign(self.weights))
+        gradAllWeights = np.where(np.abs(self.weights) < 1 / self.weight_decay, self.weight_decay * self.weights,
+                                  np.sign(self.weights))
 
-    print(gradAllWeights)
-    return [[self.weights, gradAllWeights], self.name]
+        print(gradAllWeights)
+        return [[self.weights, gradAllWeights], self.name]
--- regularizers/L1Regularizer.py
+++ regularizers/L1Regularizer.py
@@ -1,20 +1,21 @@
 import numpy as np
 from regularizers.Regularizer import *
 
+
 class L1Regularizer(Regularizer):
 
-  def forward(self):
-    """
-     Returns:
-      Scalar, loss due to the L2 regularization.
-    """
-    # regularization term loss portion of L1 loss = lambda * sum(abs(weight))
-    return self.weight_decay * np.sum(np.abs(self.weights))
+    def forward(self):
+        """
+        Returns:
+          Scalar, loss due to the L2 regularization.
+        """
+        # regularization term loss portion of L1 loss = lambda * sum(abs(weight))
+        return self.weight_decay * np.sum(np.abs(self.weights))
 
-  def backward_params(self):
-    """
-    Returns:
-      Gradient of the L1 loss with respect to the regularized weights.
-    """
-    grad_weights = self.weight_decay * np.sign(self.weights)
-    return [[self.weights, grad_weights], self.name]
+    def backward_params(self):
+        """
+        Returns:
+          Gradient of the L1 loss with respect to the regularized weights.
+        """
+        grad_weights = self.weight_decay * np.sign(self.weights)
+        return [[self.weights, grad_weights], self.name]
