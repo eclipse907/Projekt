b8b3ac33959ebc715214b3d9cf557cf3bceddde2
Iva Panic <iva.panic@fer.hr>
2020-01-10 15:28:26 +0100

    popravljen bug

--- PolynomialFunction.py
+++ PolynomialFunction.py
@@ -16,8 +16,8 @@ class PolynomialFunction:
             self.letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
                         'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
             self.__parse__(val, self.letters)
-        elif isinstance(val, []):
-            self.coefficients = val
+
+
 
 
     def __parse__(self, function, letters):
