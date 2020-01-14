9cb1bad1a3a27681fbb0dd3205fe0539ff086cd6
Iva Panic <iva.panic@fer.hr>
2020-01-14 13:48:43 +0100

    popravljen bug

--- Main.py
+++ Main.py
@@ -22,6 +22,8 @@ def check_arguments(options):
             options.initial_point = 10
         if options.iterations is None:
             options.iterations = 200
+        if options.alpha is None:
+           # options.alpha = Hrvoje odaberi
     elif options.algorithm == "ADAM":
         if options.learning_rate is None:
             options.learning_rate = 0.02
@@ -29,14 +31,22 @@ def check_arguments(options):
             options.initial_point = 10
         if options.iterations is None:
             options.iterations = 200
+        if options.beta_1 is None:
+            # options.beta_1 = Hrvoje odaberi
+        if options.beta_2 is None:
+            # options.beta_2 = Hrvoje odaberi
     else:
         raise Exception('Illegal algorithm - choose a valid algorithm [SGD, SGDM, ADAM]')
+    return options
+
+
 
 
 def main():
 
     parser = argparse.ArgumentParser(
-        description='Choose an optimising algorithm (-algo / --algorithm), function to optimise (-if / --input_function) and (optional) parameters: learning rate (-lr / --learning_rate), initial point (-ip / --initial_point) and number of iterations (-it / --iterations): ')
+        description='Choose an optimising algorithm (-algo / --algorithm), function to optimise (-if / --input_function) and (optional) parameters: learning rate (-lr / --learning_rate), '
+                    'initial point (-ip / --initial_point), number of iterations (-it / --iterations), momentum (-alpha) for SGD and decays (-b1, -b2) for ADAM: ')
     parser.add_argument('-algo', '--algorithm', type=str, action="store", nargs='?', help="Chosen algorithm",
                         dest="algorithm")
     parser.add_argument('-if', '--input_function', type=str, action="store", nargs='?', help="Function to optimise",
@@ -47,21 +57,31 @@ def main():
                         help="Initial point of the algorithm", dest="initial_point")
     parser.add_argument('-it', '--iterations', type=int, action="store", nargs='?',
                         help="Number of iterations for the algorithm", dest="iterations")
+    parser.add_argument('-alpha', '--alpha', type=float, action="store", nargs='?', help="Chosen momentum", dest="alpha")
+    parser.add_argument('-b1', '--beta_1', type=float, action="store", nargs='?', dest="beta_1", help="Chosen exponential decay for the first moment")
+    parser.add_argument('-b2', '--beta_2', type=float, action="store", nargs='?', dest="beta_2", help="Chosen exponential decay rate for the second moment")
 
-    print("Choose an optimising algorithm (-algo / --algorithm), function to optimise (-if / --input_function) and (optional) parameters: learning rate (-lr / --learning_rate), initial point (-ip / --initial_point) and number of iterations (-it / --iterations):")
     options = parser.parse_args()
 
-    check_arguments(options)
+    options = check_arguments(options)
 
     function = PolynomialFunction(options.input_function)
     algorithm = options.algorithm
     learning_rate = options.learning_rate
     initial_point = options.initial_point
     num_of_iterations = options.iterations
+    alpha = options.alpha
+    beta_1 = options.beta_1
+    beta_2 = options.beta_2
 
     print("Given function: " + str(
         options.input_function) + " Choosen algorithm: " + algorithm + " Learning rate: " + str(
         learning_rate) + " Inital point: " + str(initial_point) + " Number of iterations: " + str(num_of_iterations))
+    if algorithm == "SGDM":
+        print(" Momentum: " + algorithm)
+    if algorithm == "ADAM":
+        print(" Exponential decays for first and second momentum: " + beta_1 + ", " + beta_2)
+
     print(function.__call__(opt.optimise(algorithm, function, learning_rate, initial_point, num_of_iterations)))
 
 
