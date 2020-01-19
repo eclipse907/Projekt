fa9c0673396a7506a98a03009fdd42667bd774ed
Leo Pleše <leo.plese@fer.hr>
2020-01-19 14:41:34 +0100

    fix3 in earlyStopping

--- regularizers/earlyStopping.py
+++ regularizers/earlyStopping.py
@@ -8,13 +8,12 @@ def earlyStopping(X_train, Y_train, n, p, subtrain_valid_split_factor = 0.7):
     outSet = inOutSets[1]
 
     # set theta0
-    W1, b1, W2, b2 = fcann2_setup_initial_params(inSet[1], outSet[1])
+    W1, b1, W2, b2 = fcann2_setup_initial_params(X_train, Y_train)
     # find optimal params by early stopping
     opt_params, opt_niter, opt_error = findOptimalParams(W1, b1, W2, b2, inSet, outSet, n, p)
 
 
-    # set theta to random values again - TODO: check
-    fcann2_setup_initial_params(X_train, Y_train)
+    # set theta to random values again - TODO: not needed because W1, b1, W2, b2 are saved (line 11)?
 
     W1, b1, W2, b2 = fcann2_train(X_train, Y_train, opt_niter)
 
