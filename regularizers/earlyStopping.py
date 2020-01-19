4cb573b47491bc29cb61468df2694bd5325953d7
Leo Pleše <leo.plese@fer.hr>
2020-01-19 15:58:20 +0100

    add param setup before last train in earlyStopping

--- regularizers/earlyStopping.py
+++ regularizers/earlyStopping.py
@@ -14,7 +14,8 @@ def earlyStopping(X_train, Y_train, n, p, subtrain_valid_split_factor = 0.7):
     # find optimal params by early stopping
     opt_params, opt_niter, opt_error = findOptimalParams(W1, b1, W2, b2, inSet, outSet, n, p)
 
-    # set theta to random values again - not needed because W1, b1, W2, b2 are saved (line 11)
+    # set theta to random values again
+    W1, b1, W2, b2 = fcann2_setup_initial_params(X_train, Y_train)
     W1, b1, W2, b2 = fcann2_train(X_train, Y_train, opt_niter) if opt_niter else fcann2_train(X_train, Y_train)
 
 
