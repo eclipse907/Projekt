d452b2ad56c3f43233292c9a976591a5c30e1609
Pavo98 <Pavo.Matanovic@fer.hr>
2020-01-20 23:15:52 +0100

    Dodan CrossEntropy

---
+++ losses/CrossEntropyLoss.py
@@ -0,0 +1,21 @@
+import numpy as np
+from losses.LossFunction import LossFunction
+
+
+class Loss(LossFunction):
+
+    def forward(self):
+        self.scores = self.model.scores2
+        self.Y_oh = np.zeros(self.scores.shape)
+        self.Y_oh[range(self.scores.shape[0]), self.Y_] = 1
+        self.probs = self.stable_softmax(self.scores)
+        logprobs = np.log(self.probs[range(self.model.N), self.model.Y_])  # N x 1
+        regularized_loss = -(np.sum(logprobs) / self.model.N)  # skalar
+        if self.regularizers:
+            for reg in self.regularizers:
+                regularized_loss += reg.forward()
+        return regularized_loss
+
+    def backward_inputs(self):
+        dL_ds = self.probs - self.Y_oh  # N x C
+        return dL_ds
