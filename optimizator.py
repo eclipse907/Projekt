09fd7cdfa4ef5c5147280e2a2ac3c7c6f34203d6
Maligetzus <hrvoje.maligec@fer.hr>
2020-01-22 17:33:15 +0100

    sitni popravci u optimizatoru, ADAM i SGD

--- optimizator.py
+++ optimizator.py
@@ -12,7 +12,8 @@ class Optimizator:
         self.domain_point_derivative_2 = 0
         self.first_moment_vector_1 = 0
         self.first_moment_vector_2 = 0
-        self.second_moment_vector = 0
+        self.second_moment_vector_1 = 0
+        self.second_moment_vector_2 = 0
 
     def __call__(self, grad_W1, grad_W2):
         self.iterations += 1
@@ -30,8 +31,10 @@ class Optimizator:
         return w1, w2
 
     def __sgdm(self, grad_W1, grad_W2):
-        self.domain_point_derivative_1 = self.params.momentum * self.domain_point_derivative_1 - self.params.learning_rate_sgdm * grad_W1
-        self.domain_point_derivative_1 = self.params.momentum * self.domain_point_derivative_2 - self.params.learning_rate_sgdm * grad_W2
+        self.domain_point_derivative_1 = \
+            self.params.momentum * self.domain_point_derivative_1 - self.params.learning_rate_sgdm * grad_W1
+        self.domain_point_derivative_2 = \
+            self.params.momentum * self.domain_point_derivative_2 - self.params.learning_rate_sgdm * grad_W2
 
         w1 = np.transpose(self.model.W1) + self.domain_point_derivative_1
         w2 = np.transpose(self.model.W2) + self.domain_point_derivative_2
@@ -39,17 +42,26 @@ class Optimizator:
         return w1, w2
 
     def __adam(self, grad_W1, grad_W2):
-        self.first_moment_vector_1 = self.params.beta_1 * self.first_moment_vector_1 + (1 - self.params.beta_1) * grad_W1
-        self.first_moment_vector_2 = self.params.beta_1 * self.first_moment_vector_2 + (1 - self.params.beta_1) * grad_W2
-        self.second_moment_vector_1 = self.params.beta_2 * self.second_moment_vector + (1 - self.params.beta_2) * np.power(grad_W1, 2)
-        self.second_moment_vector_2 = self.params.beta_2 * self.second_moment_vector + (1 - self.params.beta_2) * np.power(grad_W2, 2)
-        computational_first_moment_vector = self.first_moment_vector_1 / (1 - np.power(self.params.beta_1, self.iterations + 1))
-        computational_second_moment_vector = \
-            self.second_moment_vector / (1 - np.power(self.params.beta_2, self.iterations + 1))
+        self.first_moment_vector_1 = \
+            self.params.beta_1 * self.first_moment_vector_1 + (1 - self.params.beta_1) * grad_W1
+        self.first_moment_vector_2 = \
+            self.params.beta_1 * self.first_moment_vector_2 + (1 - self.params.beta_1) * grad_W2
+        self.second_moment_vector_1 = \
+            self.params.beta_2 * self.second_moment_vector_1 + (1 - self.params.beta_2) * np.power(grad_W1, 2)
+        self.second_moment_vector_2 = \
+            self.params.beta_2 * self.second_moment_vector_2 + (1 - self.params.beta_2) * np.power(grad_W2, 2)
+        computational_first_moment_vector_1 = \
+            self.first_moment_vector_1 / (1 - np.power(self.params.beta_1, self.iterations + 1))
+        computational_first_moment_vector_2 = \
+            self.first_moment_vector_2 / (1 - np.power(self.params.beta_1, self.iterations + 1))
+        computational_second_moment_vector_1 = \
+            self.second_moment_vector_1 / (1 - np.power(self.params.beta_2, self.iterations + 1))
+        computational_second_moment_vector_2 = \
+            self.second_moment_vector_2 / (1 - np.power(self.params.beta_2, self.iterations + 1))
 
-        w1 = np.transpose(self.model.W1) - self.params.learning_rate * computational_first_moment_vector / \
-                       (np.sqrt(computational_second_moment_vector) + self.params.epsilon)
-        w2 = np.transpose(self.model.W2) - self.params.learning_rate * computational_first_moment_vector / \
-                       (np.sqrt(computational_second_moment_vector) + self.params.epsilon)
+        w1 = np.transpose(self.model.W1) - self.params.learning_rate * computational_first_moment_vector_1 / \
+                       (np.sqrt(computational_second_moment_vector_1) + self.params.epsilon)
+        w2 = np.transpose(self.model.W2) - self.params.learning_rate * computational_first_moment_vector_2 / \
+                       (np.sqrt(computational_second_moment_vector_2) + self.params.epsilon)
 
         return w1, w2
