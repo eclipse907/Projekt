a655b06dbad4d37cade3b8b5d90840d336b579a4
eclipse907 <tin.reiter@gmail.com>
2020-01-23 21:34:39 +0100

    Dodano testiranje na setu za testiranjw

--- fcann2.py
+++ fcann2.py
@@ -167,4 +167,14 @@ if __name__ == "__main__":
     probs = lossClass.get_probs_from_scores(model.scores2)
     Y = np.argmax(probs, axis=1)
     accuracy, recall, precision = data.eval_perf_multi(Y, y_train)
-    print(accuracy, recall, precision)
+    print(accuracy)
+    print(precision)
+    print(recall)
+    N_test = x_test.shape[0]
+    D_test = x_test.shape[1] * x_test.shape[2]
+    C_test = np.max(y_test) + 1
+    x_test = np.reshape(x_test, (N_test, D_test))
+    model.forward_pass(x_test)
+    probs = lossClass.get_probs_from_scores(model.scores2)
+    Y = np.argmax(probs, axis=1)
+    accuracy, recall, precision = data.eval_perf_multi(Y, y_test)
